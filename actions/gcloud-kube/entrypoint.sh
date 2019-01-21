623f43c94a13f1263f27a04e504442ed97f68b97
Komin Antony <3514045+komony@users.noreply.github.com>
2019-01-21 16:37:19 -0500

    Adding gcloud detailed config options

--- actions/gcloud-kube/entrypoint.sh
+++ actions/gcloud-kube/entrypoint.sh
@@ -2,6 +2,9 @@
 
 set +x
 
+gcloud config set project qualified-smile-226721
+gcloud config set compute/zone us-east4-a
+
 gcloud container clusters create hello-cluster --num-nodes=1
 kubectl run hello-web --image=kbhai/actions:test --port 8080
 kubectl get pods
