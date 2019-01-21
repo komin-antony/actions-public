b890665dd643cb822b7e8ff642068997a361e48b
Komin Antony <3514045+komony@users.noreply.github.com>
2019-01-21 18:22:15 -0500

    Removing debugging opts

--- actions/gcloud-kube/entrypoint.sh
+++ actions/gcloud-kube/entrypoint.sh
@@ -7,5 +7,4 @@ gcloud config set compute/zone us-east4-a
 
 gcloud container clusters create hello-cluster --num-nodes=1
 kubectl run hello-web --image=kbhai/actions:test --port 8080
-kubectl get pods
 kubectl expose deployment hello-web --type=LoadBalancer --port 80 --target-port 8080
