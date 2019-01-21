6fdd2d38f6bbbbdcc58999294299522b9e358950
Komin Antony <3514045+komony@users.noreply.github.com>
2019-01-21 18:47:20 -0500

    Adding logic for rolling deployments

--- actions/gcloud-kube/entrypoint.sh
+++ actions/gcloud-kube/entrypoint.sh
@@ -5,6 +5,13 @@ set +x
 gcloud config set project qualified-smile-226721
 gcloud config set compute/zone us-east4-a
 
-gcloud container clusters create hello-cluster --num-nodes=1
-kubectl run hello-web --image=kbhai/actions:test --port 8080
-kubectl expose deployment hello-web --type=LoadBalancer --port 80 --target-port 8080
+string=$(kubectl get deployment hello-web)
+if [[ $string == *"Error"* ]]; then
+  gcloud container clusters create hello-cluster --num-nodes=1
+  kubectl run hello-web --image=kbhai/actions:test --port 8080
+  kubectl expose deployment hello-web --type=LoadBalancer --port 80 --target-port 8080
+else
+  kubectl set image deployment/hello-web hello-web=kbhai/actions:test
+fi
+
+
