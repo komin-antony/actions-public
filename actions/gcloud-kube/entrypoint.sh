1e98d0228f80f77f0fbf36b7310e7b894ce63ca8
Komin Antony <komin.antony@capitalone.com>
2019-01-22 11:50:24 -0500

    Force cleanup clusters

--- actions/gcloud-kube/entrypoint.sh
+++ actions/gcloud-kube/entrypoint.sh
@@ -5,13 +5,9 @@ set +x
 gcloud config set project qualified-smile-226721
 gcloud config set compute/zone us-east4-a
 
-string=$(gcloud container clusters create hello-cluster --num-nodes=1)
-if [[ $string == *"Error"* ]]; then
-  gcloud container clusters get-credentials hello-cluster
-  kubectl set image deployment/hello-web hello-web=kbhai/actions:google
-else
-  kubectl run hello-web --image=kbhai/actions:google --port 443
-  kubectl expose deployment hello-web --type=LoadBalancer --port 443 --target-port 443
-fi
+gcloud container clusters delete hello-cluster --num-nodes=1
+gcloud container clusters create hello-cluster --num-nodes=1
+kubectl run hello-web --image=kbhai/actions:google --port 443
+kubectl expose deployment hello-web --type=LoadBalancer --port 443 --target-port 443
 
 
