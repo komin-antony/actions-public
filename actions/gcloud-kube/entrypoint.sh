6e5fcd3a0297ed642b6d0ae46d205dc3d32c0ae6
Komin <kominbhai@gmail.com>
2019-01-21 21:02:15 -0500

    Cleaning up GCloud deployment kube order

--- actions/gcloud-kube/entrypoint.sh
+++ actions/gcloud-kube/entrypoint.sh
@@ -5,14 +5,13 @@ set +x
 gcloud config set project qualified-smile-226721
 gcloud config set compute/zone us-east4-a
 
-gcloud container clusters get-credentials hello-cluster
-string=$(kubectl get deployment hello-web)
+string=$(gcloud container clusters create hello-cluster --num-nodes=1)
 if [[ $string == *"Error"* ]]; then
-  gcloud container clusters create hello-cluster --num-nodes=1
+  gcloud container clusters get-credentials hello-cluster
+  kubectl set image deployment/hello-web hello-web=kbhai/actions:google
+else
   kubectl run hello-web --image=kbhai/actions:google --port 8080
   kubectl expose deployment hello-web --type=LoadBalancer --port 80 --target-port 8080
-else
-  kubectl set image deployment/hello-web hello-web=kbhai/actions:google
 fi
 
 
